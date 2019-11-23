a1873fd9adb9469c0ff4dc9de3038d47a5d0e75e
Max <max@birkett.pl>
2019-11-23 20:34:01 +0100

    Edited alot of things

--- map-infobox-data.js
+++ map-infobox-data.js
@@ -1,19 +1,19 @@
 var AfricaPrograms = {
 	"bw": {
-		"test3": ["bla bla bla", "https://www.google.com"]
+		"test3": []
 	},
 };
 
 var EuropePrograms = {
 	"pl": {
-		"test1": ["Copenhagen Business School", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/denmark/cbs"],
+		"test1": [],
 	},
 };
 
 var AsiaPrograms = {
 	"bd" : {
-		"test1": ["Chinese University of Hong Kong", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/cuhk", "City University of Hong Kong", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/cityu", "Hong Kong University of Science and Technology", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/hkust", "Nanjing University", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/nanjing", "Peking University", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/peking", "Soochow University", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/soochow", "Tsinghua University", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/tsinghua", "USTC", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/ustc", "Zhongnan University of Economics and Law", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/yearsemester/china/zuel"],
-		"test3": ["Chinese University of Hong Kong Summer School", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/china/cuhk", "Higher Education Administration Program in China (Grad Students)", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/china/highered", "Hong Kong University of Science and Technology", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/china/hkust", "Journalism Without Walls", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/journalism/china", "Shanghai", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/china/shanghai", "Wuhan", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/china/wuhan"]
+		"test1": [],
+		"test3": []
 	},
 };
 
@@ -22,12 +22,12 @@ var AustraliaPrograms = {
 
 var NAPrograms = {
 	"ca": {
-		"test2": ["Costra Rica", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/winter/costarica.html"]
+		"test2": []
 	}
 };
 
 var SAPrograms = {
 	"br": {
-		"test3": ["Summer in Ecuador", "http://www.stonybrook.edu/commcms/studyabroad/outgoing/programs/summer/ecuador/ecuador"]
+		"test3": []
 	}
 };
