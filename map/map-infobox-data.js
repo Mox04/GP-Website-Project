f421c4be5ab8a571f4036673af06ed8fb09389a3
Max <max@birkett.pl>
2019-11-27 20:58:34 +0100

    filled in information (some missing)

--- map/map-infobox-data.js
+++ map/map-infobox-data.js
@@ -1,19 +1,24 @@
 var AfricaPrograms = {
 	"bw": {
-		"test3": []
+		"link": ["Link to Country page","botswana.html"],
+		"progdesc": ["Orphan Care Program:<br>The Batswana program aims to aid the orphan child population. The program supports children institutionally and materially (monthly food basket between $60 and $80).", "placeholder"],
+		"stat": ["Number of Beneficiaries: 35 thous [2015]<sup>17</sup>","placeholder","Total Annual Spending: $33 mil [2015]<sup>17</sup>","placeholder"],
 	},
 };
 
 var EuropePrograms = {
 	"pl": {
-		"test1": [],
+		"link": ["Link to Country page","poland.html"],
+		"progdesc": ["Family 500+:<br>The program is a monthly, tax-free payment worth $130 The family receives for every child below the age of 18, regardless of income and other factors. Payments range from $0 to $6,500 annually", "placeholder"],
+		"stat": ["Number of Beneficiaries: 3 mil [2018]<sup>16</sup>","placeholder","Total Annual Spending: $5,5 bil [2018]<sup>16</sup>","placeholder"],
 	},
 };
 
 var AsiaPrograms = {
 	"bd" : {
-		"test1": [],
-		"test3": []
+		"link": ["Link to Country page","bangladesh.html"],
+		"progdesc": ["...:<br>...", "placeholder"], //INFO MISSING//
+		"stat": ["Number of Beneficiaries: ...<sup>...</sup>","placeholder","Total Annual Spending: ... <sup>...</sup>","placeholder"],
 	},
 };
 
@@ -22,12 +27,16 @@ var AustraliaPrograms = {
 
 var NAPrograms = {
 	"ca": {
-		"test2": []
+		"link": ["Link to Country page","canada.html"],
+		"progdesc": ["Canadian Child Benefit:<br>The program is a monthly, tax-free payment to help with the costs of raising children. The values of the payment depend on factors such as child age and household income.", "placeholder"],
+		"stat": ["Number of Beneficiaries: 7 mil [2017]<sup>14</sup>","placeholder","Total Annual Spending: $17 mil [2017]<sup>14</sup>","placeholder"],
 	}
 };
 
 var SAPrograms = {
 	"br": {
-		"test3": []
+		"link": ["Link to Country page","brazil.html"],
+		"progdesc": ["Bolsa Família:<br>...", "placeholder"], //INFO MISSING//
+		"stat": ["Number of Beneficiaries: 17 mil [2017]<sup>13</sup>","placeholder","Total Annual Spending: $8.84 mil [2017]<sup>13</sup>","placeholder"],
 	}
 };
