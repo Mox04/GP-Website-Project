2e1e1b4a7b561f2bc1a2807b2ade79c16da42784
Max <max@birkett.pl>
2019-11-27 21:01:09 +0100

    underlined program names

--- map/map-infobox-data.js
+++ map/map-infobox-data.js
@@ -1,7 +1,7 @@
 var AfricaPrograms = {
 	"bw": {
 		"link": ["Link to Country page","botswana.html"],
-		"progdesc": ["Orphan Care Program:<br>The Batswana program aims to aid the orphan child population. The program supports children institutionally and materially (monthly food basket between $60 and $80).", "placeholder"],
+		"progdesc": ["<u>Orphan Care Program:</u><br>The Batswana program aims to aid the orphan child population. The program supports children institutionally and materially (monthly food basket between $60 and $80).", "placeholder"],
 		"stat": ["Number of Beneficiaries: 35 thous [2015]<sup>17</sup>","placeholder","Total Annual Spending: $33 mil [2015]<sup>17</sup>","placeholder"],
 	},
 };
@@ -9,7 +9,7 @@ var AfricaPrograms = {
 var EuropePrograms = {
 	"pl": {
 		"link": ["Link to Country page","poland.html"],
-		"progdesc": ["Family 500+:<br>The program is a monthly, tax-free payment worth $130 The family receives for every child below the age of 18, regardless of income and other factors. Payments range from $0 to $6,500 annually", "placeholder"],
+		"progdesc": ["<u>Family 500+:</u><br>The program is a monthly, tax-free payment worth $130 The family receives for every child below the age of 18, regardless of income and other factors. Payments range from $0 to $6,500 annually", "placeholder"],
 		"stat": ["Number of Beneficiaries: 3 mil [2018]<sup>16</sup>","placeholder","Total Annual Spending: $5,5 bil [2018]<sup>16</sup>","placeholder"],
 	},
 };
@@ -17,7 +17,7 @@ var EuropePrograms = {
 var AsiaPrograms = {
 	"bd" : {
 		"link": ["Link to Country page","bangladesh.html"],
-		"progdesc": ["...:<br>...", "placeholder"], //INFO MISSING//
+		"progdesc": ["<u>...:</u><br>...", "placeholder"], //INFO MISSING//
 		"stat": ["Number of Beneficiaries: ...<sup>...</sup>","placeholder","Total Annual Spending: ... <sup>...</sup>","placeholder"],
 	},
 };
@@ -28,7 +28,7 @@ var AustraliaPrograms = {
 var NAPrograms = {
 	"ca": {
 		"link": ["Link to Country page","canada.html"],
-		"progdesc": ["Canadian Child Benefit:<br>The program is a monthly, tax-free payment to help with the costs of raising children. The values of the payment depend on factors such as child age and household income.", "placeholder"],
+		"progdesc": ["<u>Canadian Child Benefit:</u><br>The program is a monthly, tax-free payment to help with the costs of raising children. The values of the payment depend on factors such as child age and household income.", "placeholder"],
 		"stat": ["Number of Beneficiaries: 7 mil [2017]<sup>14</sup>","placeholder","Total Annual Spending: $17 mil [2017]<sup>14</sup>","placeholder"],
 	}
 };
@@ -36,7 +36,7 @@ var NAPrograms = {
 var SAPrograms = {
 	"br": {
 		"link": ["Link to Country page","brazil.html"],
-		"progdesc": ["Bolsa Família:<br>...", "placeholder"], //INFO MISSING//
+		"progdesc": ["<u>Bolsa Família:</u><br>...", "placeholder"], //INFO MISSING//
 		"stat": ["Number of Beneficiaries: 17 mil [2017]<sup>13</sup>","placeholder","Total Annual Spending: $8.84 mil [2017]<sup>13</sup>","placeholder"],
 	}
 };
